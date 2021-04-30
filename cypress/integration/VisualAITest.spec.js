0ff4b46fcb815e160bf26a056988051bd5d06f87
Eric Gibby <eric.gibby@imaginelearning.com>
2021-04-30 13:10:02 -0600

    Get team name from env config + formatting

--- cypress/integration/VisualAITest.spec.js
+++ cypress/integration/VisualAITest.spec.js
@@ -1,49 +1,49 @@
 /// <reference types="cypress" />
+const TEAM_NAME = process.env.TEAM_NAME || '[Team Name]';
 
+describe('Visual Validation Applitools', () => {
+	before(function () {
+		cy.visit(Cypress.config('baseUrl'));
+	});
 
+	beforeEach(function () {
+		cy.eyesOpen({
+			appName: `ApplitoolsPalooza - ${TEAM_NAME}`,
+			testName: this.currentTest.title,
+		});
+	});
 
-describe('Visual Validation Applitools', () =>{ 
-   
-    before(function() {
-        cy.visit(Cypress.config('baseUrl'));
-        
-    });
-    beforeEach(function() {
-        cy.eyesOpen({
-            appName: 'ApplitoolsPalooza - [Team Name]',
-            testName: this.currentTest.title,
-        })
-    })
-    afterEach(() => {
-        cy.eyesClose()
-    }) 
+	afterEach(() => {
+		cy.eyesClose();
+	});
 
-    it('Display Elements of Authentification Page', () => {
-        // visual Validation
-    })
+	it('Display Elements of Authentification Page', () => {
+		// visual Validation
+	});
+
+	it('Login Attempt no username and no password', () => {
+		cy.get('#log-in').click();
+		// visual Validation
+	});
+
+	it('Login Attempt Only Username', () => {
+		cy.get('#username').type('demo@applitools.com');
+		cy.get('#log-in').click();
+		// visual Validation
+		cy.get('#username').clear();
+	});
+
+	it('Login Attempt Only Password', () => {
+		cy.get('#password').type('password');
+		cy.get('#log-in').click();
+		// visual Validation
+		cy.get('#password').clear();
+	});
     
-
-    it('Login Attempt no username and no password', () =>{
-        cy.get("#log-in").click();
-        // visual Validation
-    
-    })
-    it('Login Attempt Only Username', () => {
-        cy.get("#username").type("demo@applitools.com")
-        cy.get("#log-in").click()
-        // visual Validation
-        cy.get("#username").clear()
-    })
-    it('Login Attempt Only Password', () => {
-        cy.get("#password").type("password")
-        cy.get("#log-in").click();
-        // visual Validation
-        cy.get("#password").clear()
-    })
-    it('Login Attempt Success', () => {
-        cy.get("#username").type("demo@applitools.com")
-        cy.get('#password').type("password")
-        cy.get("#log-in").click();
-        // visual Validation
-    })
-})
+	it('Login Attempt Success', () => {
+		cy.get('#username').type('demo@applitools.com');
+		cy.get('#password').type('password');
+		cy.get('#log-in').click();
+		// visual Validation
+	});
+});
